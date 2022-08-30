4c2b92908277ef9a5dcb16fb6e43d4d721a72206
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-08-30 15:04:47 +0300

    Commit 3 | Java/String/ Pattern Syntax Checker Solved

---
+++ Java/Strings/Pattern Syntax Checker.java
@@ -0,0 +1,46 @@
+/*
+Input Format
+
+The first line of input contains an integer "N" denoting the number of test cases. 
+The next "N" lines contain a string of any printable characters representing the pattern of a regex.
+
+Output Format
+
+For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
+
+Sample Input
+
+3
+([A-Z])(.+)
+[AZ[a-z](a-z)
+batcatpat(nat
+
+Sample Output
+
+Valid
+Invalid
+Invalid
+*/
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row = Integer.parseInt(scan.nextLine());
+        while(row>0){
+            String line = scan.nextLine();
+            try{
+                Pattern p = Pattern.compile(line);
+                System.out.println("Valid");
+            }catch(Throwable t){
+                System.out.println("Invalid");
+            }
+            row--;
+      }
+    }
+}
+
