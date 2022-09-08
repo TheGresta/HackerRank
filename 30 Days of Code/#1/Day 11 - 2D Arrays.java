0ff71629caa42a8edd0c175c0530f37fe6a1ab6e
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-08 11:58:09 +0300

    Commit 18 | 30 Day of Code #1 Day 11 Solved

---
+++ 30 Days of Code/#1/Day 11 - 2D Arrays.java
@@ -0,0 +1,102 @@
+/*
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+a b c
+  d
+e f g
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+
+19
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+2 4 4
+  2
+1 2 4
+
+
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void prt(List<List<Integer>> arr){
+        int max = Integer.MIN_VALUE;
+        for(int i=1; i<arr.size()-1; i++){
+            for(int j=1; j<arr.get(0).size()-1; j++){
+            int sum = 0;
+            sum += arr.get(i).get(j);
+            sum += arr.get(i-1).get(j-1);
+            sum += arr.get(i-1).get(j);
+            sum += arr.get(i-1).get(j+1);
+            sum += arr.get(i+1).get(j-1);
+            sum += arr.get(i+1).get(j);
+            sum += arr.get(i+1).get(j+1);
+            max = (max < sum) ? sum : max;
+            }        
+        }
+        System.out.println(max);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        
+        prt(arr);
+    }
+}
