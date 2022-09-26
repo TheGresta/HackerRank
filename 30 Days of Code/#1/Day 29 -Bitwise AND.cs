a35ffe9619fd01f72bedf00573281197b81f6e4c
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-27 01:05:21 +0300

    Commit 39 | 30 Day of Code #1 Day 29 Solved

---
+++ 30 Days of Code/#1/Day 29 -Bitwise AND.cs
@@ -0,0 +1,115 @@
+/*
+Task
+Given set S = {1, 2, 3, ...., N}. Find two integers, A and B (where A < B), 
+from set S such that the value of A&B is the maximum possible and also less than a given integer, K. 
+In this case, & represents the bitwise AND operator.
+
+Function Description
+
+Complete the bitwiseAnd function in the editor below.
+
+bitwiseAnd has the following paramter(s):
+- int N: the maximum integer to consider
+- int K: the limit of the result, inclusive
+
+Sample Input
+
+STDIN   Function
+-----   --------
+3       T = 3
+5 2     N = 5, K = 2
+8 5     N = 8, K = 5
+2 2     N = 8, K = 5
+
+Sample Output
+
+1
+4
+0
+*/
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K)
+    {
+        int max = 0;
+        
+        for(int i=1; i<=N; i++){
+            for(int j=i+1; j<=N; j++){                
+                int tmp = i&j;                
+                if(tmp < K && tmp > max)
+                    max = tmp;
+            }
+        }
+        
+        return max;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int count = Convert.ToInt32(firstMultipleInput[0]);
+
+            int lim = Convert.ToInt32(firstMultipleInput[1]);
+
+            int res = Result.bitwiseAnd(count, lim);
+
+            textWriter.WriteLine(res);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
